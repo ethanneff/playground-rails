c48fbc5bb5598b6f20d27093d4ccc1f29a491be5
Ethan Neff <ethan.neff@eneff.com>
2021-02-23 21:31:12 -0800

    fix: add auth for friend ids

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,6 +3,8 @@
 # entry to Friends
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :correct_user, only: %i[edit update destroy]
 
   # GET /friends or /friends.json
   def index
@@ -14,7 +16,7 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -22,7 +24,7 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -55,6 +57,11 @@ class FriendsController < ApplicationController
       format.html { redirect_to friends_url, notice: 'Friend was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: 'Not authorized to edit this friend' if @friend.nil?
   end
 
   private
